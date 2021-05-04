f130566f23dd693047f07fbadb6b9f55f5aae5a2
Alexandre <alexandre.cardozo1@gmail.com>
2021-05-04 20:44:38 -0300

    removed files

--- src/services/dataApi.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const dataApi = axios.create({
-  baseURL: 'https://zzrl-034.sandbox.us01.dx.commercecloud.salesforce.com/s/Sites-Site/'
-});
-
-export default dataApi;
